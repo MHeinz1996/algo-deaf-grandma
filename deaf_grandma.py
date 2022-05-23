bc816943baf5b81d2d7f55bf451cb7a1d76a86dc
MHeinz1996 <michael.heinzinger@gmail.com>
2022-05-23 15:13:49 -0500

    Completed deaf_grandma.py

--- deaf_grandma.py
+++ deaf_grandma.py
@@ -1,4 +1,26 @@
 def deaf_grandma():
+    goodbye_count = 0
+    
+    print("HEY KID!")
+
+    while(goodbye_count < 2):
+        x = input('> ')
+
+        if(x == ""):
+            print("WHAT?!")
+        else:
+            if(x.isupper() == False):
+                print("SPEAK UP, KID!")
+            elif(x == 'GOODBYE!'):
+                if(goodbye_count < 1):
+                    print("LEAVING SO SOON?")
+                    goodbye_count += 1
+                else:
+                    print("LATER, SKATER!")
+                    goodbye_count += 1
+            else:
+                print("NO, NOT SINCE 1946!")
+
 
 
 
