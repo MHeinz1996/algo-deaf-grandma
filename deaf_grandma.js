c14f7860dd5d74e4a5b4ad0a56d2133cc6045d12
MHeinz1996 <michael.heinzinger@gmail.com>
2022-05-23 15:34:01 -0500

    Completed deaf_grandma.js

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,4 +1,31 @@
 function deafGrandma() {
+    let goodbye_count = 0;
+
+    console.log("HEY KID!");
+
+    while(goodbye_count < 2) {
+        const prompt = require("prompt-sync")({ sigint: true });
+        const x = prompt("> ");
+
+        if(x === "") {
+            console.log("WHAT?!");
+        } else {
+            if(x !== x.toUpperCase()) {
+                console.log("SPEAK UP, KID!");
+            } else if (x === "GOODBYE!") {
+                if(goodbye_count < 1) {
+                    console.log("LEAVING SO SOON?");
+                    goodbye_count++;
+                } else {
+                    console.log("LATER, SKATER!");
+                    goodbye_count++;
+                }
+            } else {
+                console.log("NO, NOT SINCE 1946!");
+            }
+        }
+
+    }
 
 }
 
